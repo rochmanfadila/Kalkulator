d81fdda1888f41aa8ee1f317e949fd4eb5ed5c00
rochmanfadila <68120070+rochmanfadila@users.noreply.github.com>
2026-01-07 07:54:05 +0700

    Ini Kalkulator saya

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+let display = document.getElementById("display");
+
 function appendValue(value) {
-    document.getElementById("display").value += value;
+  display.value += value;
 }
 
 function clearDisplay() {
-    document.getElementById("display").value = "";
+  display.value = "";
 }
 
 function calculateResult() {
-    const display = document.getElementById("display");
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = "Error";
-    }
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
 }
